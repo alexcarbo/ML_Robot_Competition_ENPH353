d9f0c46e3707e77ec9f9359ab20089069c0aa6b5
ThienH7 <thien.ho@alumni.ubc.ca>
2020-03-30 20:53:47 -0700

    Added the simulated world

---
+++ 353_ws/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/fizzer/Desktop/ML_Robot_Competition_ENPH353/353_ws/src
+# Build directory: /home/fizzer/Desktop/ML_Robot_Competition_ENPH353/353_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("competition_2019t2")
--- src/analyze_video.py
+++ src/analyze_video.py
@@ -1,115 +1,135 @@
 import cv2
 import os
 import numpy as np
+import time
 
-yellow_min = np.array([15,70,20])
-yellow_max = np.array([35,255,255])
+yellow_min = np.array([10,70,75])
+yellow_max = np.array([25,150,200])
 blue_min = np.array([100,50,50])
 blue_max = np.array([120,200,200])
 green_min = np.array([80,50,50])
 green_max = np.array([90,230,255])
-thresh = 10
+trigger_thresh = 10
+noise_thresh = 12
+binary_thresh = 40
+right_half = 320
 
 def main():
-    video_directory = "/home/fizzer/Desktop/ML_Robot_Competition_ENPH353/src/data/yellow00.avi"
+    video_directory = "/home/fizzer/Desktop/ML_Robot_Competition_ENPH353/src/data/drive0.avi"
     directory = "/home/fizzer/Desktop/ML_Robot_Competition_ENPH353/src/data/"
 
     os.chdir(directory)
     vidObj = cv2.VideoCapture(video_directory)
 
 
-    xsum, ysum, xcount, ycount, frameCount, xCenter, yCenter = 0,0,0,0,0,0,0
+    frameCount = 0
     lower_ybound, upper_ybound, upper_xbound, lower_xbound = 0, 0, 0, 0
 
     while vidObj.isOpened():
         ret, frame = vidObj.read()
-        out_array = []
+        height, width, layer = frame.shape
+        right_frame = frame[:, right_half:width]
 
         if(ret == True):
-            height, width, layer = frame.shape
+            
+            
+            # 640 x 360 pixels
             size = (width, height)
-
+            
             # Convert image to HSV and then produce a blurred binary frame
-            hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+            hsv = cv2.cvtColor(right_frame, cv2.COLOR_BGR2HSV)
             mask = cv2.inRange(hsv, yellow_min, yellow_max)
-            res = cv2.bitwise_and(frame, frame, mask=mask)
+            res = cv2.bitwise_and(right_frame, right_frame, mask=mask)
 
             greyFrame = cv2.cvtColor(res, cv2.COLOR_BGR2GRAY)
             ret, binaryFrame = cv2.threshold(greyFrame, 30, 255, cv2.THRESH_BINARY)
-            blurFrame = cv2.blur(binaryFrame, (3,3))
+            blurFrame = cv2.blur(binaryFrame, (5,5))
 
             frameArray = np.asarray(blurFrame)
 
-            vertArray = np.zeros(height)
-            horiArray = np.zeros((width))
+            horiArray = np.zeros(height)
+            vertArray = np.zeros((width))
 
-            if (frameCount < 120):
+            # Only analyze after a certain number of frames
+            if (frameCount < 0):
                 frameCount += 1
             else:
-                xsum, ysum, xcount, ycount = 0,0,0,0
-                for y in range(height):
-                    for x in range(width):
-                        if(frameArray[y][x] > 40):
-                            xsum += x
-                            ysum += y
+                # Cut frame to only the right half
+                for x in range(width - right_half):
+                    for y in range(height):
+                        # Determine important areas from vertical slices along the horizontal
+                        if(frameArray[y][x] > binary_thresh):
                             horiArray[x] += 1
-                            vertArray[y] += 1
 
-                horiArray = [0 if horiArray_ < thresh else int(horiArray_) for horiArray_ in horiArray]
-                vertArray = [0 if vertArray_ < thresh else int(vertArray_) for vertArray_ in vertArray]
-                xcount = int(np.sum(horiArray))
-                ycount = int(np.sum(vertArray))
-                xCenter = int(xsum/xcount)
-                yCenter = int(ysum/ycount)
-                frameCount = 0
+                # Filters values below a threshhold to remove noise
+                horiArray = [0 if horiArray_ < noise_thresh else int(horiArray_) for horiArray_ in horiArray]
 
-                print("Vert Array")
-                print(vertArray)
+                # print("Hori Array")
+                # print(horiArray)
                 flagFirst = False
-                lastVal = 0
-                # Vertical Bounds
-                for i in range(len(vertArray)):
-                    if (flagFirst == False and vertArray[i] > 20):
-                        print("Vert lower", i)
-                        lower_ybound = i
-                        flagFirst = True
-                    if (flagFirst == True and lastVal > vertArray[i] and vertArray[i] < 20):
-                        print("Vert upper", i)
-                        upper_ybound = i
-                        flagFirst = False
-                        lastVal = 0
-                        break
-                    lastVal = vertArray[i]
-
+                # lastVal = 0
+                
                 # Horizontal Bounds
                 for i in range(len(horiArray)):
-                    if (flagFirst == False and horiArray[i] > 20):
-                        print("Hori lower", i)
+                    if (flagFirst == False and horiArray[i] > trigger_thresh):
+                        # print("Hori lower", i)
                         lower_xbound = i
                         flagFirst = True
-                    if (flagFirst == True and lastVal > horiArray[i] and horiArray[i] < 20):
-                        print("Hori upper", i)
+                    if (flagFirst == True and horiArray[i] < trigger_thresh):
+                        # print("Hori upper", i)
                         upper_xbound = i
                         flagFirst = False
                         lastVal = 0
                         break
-                    lastVal = horiArray[i]
+                    # lastVal = horiArray[i]
+                    
+                # Determine important areas from horizontal slices along y
+                for y in range(height):
+                    for x in range(lower_xbound, upper_xbound):
+                        if(frameArray[y][x] > binary_thresh):
+                            vertArray[y] += 1
+
+                vertArray = [0 if vertArray_ < noise_thresh else int(vertArray_) for vertArray_ in vertArray]
+                
+                # print("Vert Array")
+                # print(vertArray)
+
+                # Vertical Bounds
+                for i in range(len(vertArray)):
+                    if (flagFirst == False and vertArray[i] > trigger_thresh):
+                        # print("Hori lower", i)
+                        lower_ybound = i
+                        flagFirst = True
+                    if (flagFirst == True and vertArray[i] < trigger_thresh):
+                        # print("Hori upper", i)
+                        upper_ybound = i
+                        flagFirst = False
+                        lastVal = 0
+                        break
 
 
 
+                frameCount = 0
+
+            # Draw a bounding box and circle for the center of mass
             cv2.rectangle(res, (lower_xbound, lower_ybound), (upper_xbound, upper_ybound), (0,255,0), 1)
-            cv2.circle(res, (xCenter,yCenter), 5, (0,255,0), 5)
 
-            cv2.imshow("Raw", frame)
+            print(lower_ybound)
+            print(upper_ybound)
+            car_frame = right_frame[lower_ybound:upper_ybound, lower_xbound:upper_xbound]
+
+            cv2.imshow("Raw", right_frame)
             cv2.imshow("Res", res)
-            cv2.imshow("blue", blurFrame)
+            cv2.imshow("Car", car_frame)
             cv2.waitKey(3)
+            
+            time.sleep(1)
 
-        out = cv2.VideoWriter('analyze.avi', cv2.VideoWriter_fourcc(*'DIVX'), 120, size)
+        # out = cv2.VideoWriter('analyze.avi', cv2.VideoWriter_fourcc(*'DIVX'), 120, size)
 
-        for i in range(len(out_array)):
-            out.write(out_array[i])
-        out.release
+        # for i in range(len(out_array)):
+        #     out.write(out_array[i])
+        # out.release
 
 if __name__ == "__main__":
     main()
