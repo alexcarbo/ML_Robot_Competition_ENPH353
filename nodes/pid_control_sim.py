84ed3b1d28d12d960f89079457a38c0685b3e4d7
alexcarbo <32438651+alexcarbo@users.noreply.github.com>
2020-04-13 22:04:02 -0700

    PID Control, state control for our path, crosswalk detection, pedestrain detection and avoidance.

--- nodes/pid_control_sim.py
+++ nodes/pid_control_sim.py
@@ -21,121 +21,161 @@ class LineFollower:
         self.angle = -80
         self.ignore_next = False
         self.ignore_low = False
+        self.state = 0
+        self.elestart = 0
 
 
     def process_image(self, data):
-        if self.ignore_next:
-            self.ignore_next = False
-            return
-        #do analysis
+        print("\n State: {}".format(self.state))
+        #set up publisher
         publisher = rospy.Publisher('/cmd_vel', Twist, queue_size = 1)
         move = Twist()
         bridge = CvBridge()
-        # frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        # red_ped_looker = frame[height-2*down:height-down,:]
-        # cv2.imshow("framerino", red_ped_looker)
-        # cv2.waitKey(3)
 
+        #ignore the image in the queue if have just turned - will be from old position
+        if self.ignore_next:
+            self.ignore_next = False
+            return
+        
+        if self.state == 13:
+            print("YAY")
+            move.linear.x = 0
+            move.angular.z = 1
+            publisher.publish(move)
+            return
+        #convert ROS image to openCV image, bgr format, use bottom 120 pixels for PID
         frame = bridge.imgmsg_to_cv2(data, desired_encoding="bgr8")
-        # frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        height = frame.shape[0]
-        down = 120
+        height, width, __ = frame.shape
+
+        if self.state in [6,10]:
+            #cross crosswalk
+            ped_im = frame[height-160:height-100,:]
+            if self.safe_to_go(ped_im):
+                print("FULL STEAM AHEAD")
+            else:
+                move.linear.x = 0
+                move.angular.z = 0
+                publisher.publish(move)
+                print("Man in Crosswalk")
+                return
+        
+            
+
+
+        down = 100
         small = frame[height-down:height,:]
-        # cv2.imshow("rgb", small)
-        # cv2.waitKey(3)
 
-        # Commented out by Thien
-        # hsv = cv2.cvtColor(small, cv2.COLOR_BGR2HSV)
-        # height, width, depth = hsv.shape
-        # cv2.imshow("hsv", hsv)
-        # cv2.waitKey(3)
+        #show FOV of PID
+        cv2.imshow("bgr", small)
+        cv2.waitKey(3)
 
-        # time.sleep(60)
+        #check for crosswalk
+        start_walk, see_walk = self.is_crosswalk(small)
+        if start_walk:
+            if self.state in [6,10]:
+                pass
+            elif self.state == 1:
+                print("----CROSSWALK----")
+                move.linear.x = 0
+                move.angular.z = 0
+                publisher.publish(move)
+                time.sleep(0.5)
+                self.turn_degrees(72, publisher, "r")
+                self.state += 1
+                self.ignore_next = True
+                time.sleep(0.5)
+            elif self.state in [5,9]:
+                print("----CROSSWALK----")
+                move.linear.x = 0
+                move.angular.z = 0
+                publisher.publish(move)
+                time.sleep(0.1)
+                print("Need to Cross Crosswalk")
+                self.state += 1
+                self.ignore_next = True
+                time.sleep(0.1)
+                return
+        if self.state in [6,10] and not see_walk:
+            self.state += 1
+            cv2.destroyAllWindows()
+            return
 
-        # low_red = np.array([150, 155, 84])
-        # high_red = np.array([179, 255, 255])
-        # red_mask = cv2.inRange(hsv, low_red, high_red)
-        # red = cv2.bitwise_and(hsv, hsv, mask=red_mask)
-
-        # cv2.imshow("red", red)
-        # cv2.waitKey(3)
-
+        #binary threshold image for PID processing
         lek = cv2.cvtColor(small, cv2.COLOR_RGB2GRAY)
-        ret, bottom = cv2.threshold(lek,150,255,cv2.THRESH_BINARY)
+        ret, bottom = cv2.threshold(lek,200,255,cv2.THRESH_BINARY)
         
         
 
         height, width = bottom.shape
-        xCount, yCount, num, avX, avY,y_count = 0, 0, 0, 0, 0, 0
-        max_width_l, max_width_r, max_height, min_height = 0, width, height, 0
+        xCount, yCount, num, avX, avY = 0, 0, 0, 0, 0
+        max_width_l, max_width_r = 0, width
         isYlow = False
+
+        #define the edges of the track, for finding the center of mass, then count all interior black points
         for y in range(0, height):
             max_width_l, max_width_r = 0, width
-            for x in range(0, width):
-                px = bottom[y, x]
-                if px == 255:
-                    if x < width/2 and x > max_width_l:
-                        max_width_l = x
-                    if x > width/2 and x < max_width_r :
-                        max_width_r = x
+            if self.state not in [6, 10]:
+                for x in range(0, width):
+                    px = bottom[y, x]
+                    if px == 255:
+                        if x < width/2 and x > max_width_l:
+                            max_width_l = x
+                        if x > width/2 and x < max_width_r :
+                            max_width_r = x
         
             for x in range(max_width_l, max_width_r):
+                    px = bottom[y,x]
                     if(px == 0):
                         # small[y, x] = [255, 0, 255]
                         num += 1
                         xCount += x
                         yCount += y
 
+        #"cununt" is the number of white pixels directly in front of our robot - used to know when to turn
         cununt = 0
         for y in range(0, height):
             px = bottom[y, width/2]
             if px == 255:
                 cununt += 1
-        # print("Cununt: " + str(cununt))
+        print("Cununt: " + str(cununt))
     
         
-                
+        #if any black pixels in area to search do PID otherwise keep doing what doing
         if num > 0:
-            # print("YCOUNT: " + str(y_count))
             avX = int(xCount/num)
-            # print(avX, avY)
             avY = int(yCount/num)
-            # cv2.circle(small, (avX, avY), 20, (255, 0, 255), -1)
-            # cv2.imshow("namew22", small)
-            # cv2.waitKey(3)
-            # cv2.circle(frame, (avX, avY), 20, (255, 0, 255), -1)
-            # cv2.imshow("namew", frame)
-            # cv2.waitKey(3)
 
+            #----START PID----
+            #Tuned constants, check if Y_avg is low (a sign we need to turn)
             base_speed = .2
             base_angular = 0
-            KP = .3
+            KP = .22
             KD = .05
             error = 0
             max_error = 1
             errX = -1 * (avX - width/2.0) / (width/2.0 / max_error)
-            # print(str(avY + (480-down)))
-            isYLow = (avY + 480-down > 430)
-            # if isYLow:
-            #     if(errX > 0):
-            #         error = max_error
-            #     else:
-            #         error = -1 * max_error
-            # else:
+            isYLow = (avY + 480-down > 425)
             error = errX
 
 
-            # print(error)
             if error != 0:
-                if isYLow and cununt >= 30 and self.ignore_low is False:
-
-                    print("Start")
+                #checks if Y_avg is low and lots of white pixels dead ahead, means we are at a corner and should turn
+                if (isYLow and cununt >= 30 and self.ignore_low is False and self.state not in [6,10]):
+                    time.sleep(0.1)
+                    print("Start Turn")
                     move.linear.x = 0
                     move.angular.z = 0
                     publisher.publish(move)
                     time.sleep(1)
-                    self.turn_degrees(90, publisher, error)
+                    if self.state in [0,3]:
+                        self.turn_degrees(65, publisher, "r")
+                    elif self.state == 8:
+                        self.turn_degrees(80, publisher, "r")    
+                    else:
+                        self.turn_degrees(75, publisher, "r")
+                    self.state += 1
                 else:
+                    #do PID and publish new value
                     self.ignore_low = False
                     p = KP * error
                     d = KD * abs(error-self.prev_error)
@@ -144,17 +184,28 @@ class LineFollower:
                         move.linear.x = base_speed - abs(g)
                     else:
                         move.linear.x = 0
-                    move.angular.z = 3*g
+                    move.angular.z = 2.7*g
                     publisher.publish(move)
-                    self.ignore_next = True
+            if(self.state == 11):
+                if self.elestart == 0:
+                    self.elestart = time.time()
+                print(time.time() - self.elestart)
+                if self.elestart != 0 and time.time() - self.elestart > 4.5:
+                    self.turn_degrees(85, publisher, "r")
+                    self.state += 1
+        
 
-    def turn_degrees(self, degrees, publisher, error):
+    def turn_degrees(self, degrees, publisher, direction):
         move = Twist()
         move.linear.x = 0
-        if error > 0:
-            move.angular.z = 1
-        else:
+
+        #decide which way to turn
+        if direction == "r":
             move.angular.z = -1
+        else:
+            move.angular.z = 1
+
+        #time needed to turn given degrees at 1 rad/s
         time_to = degrees / (1 * (180.0/math.pi))
         publisher.publish(move)
         self.ignore_next = True
@@ -162,10 +213,55 @@ class LineFollower:
         time.sleep(time_to)
         move.angular.z = 0
         publisher.publish(move)
-        print("End")
+        print("End Turn")
         time.sleep(1)
 
+    def is_crosswalk(self, image):
+        #use bottom 50 pixels
+        height = image.shape[0]
+        hsv = image[height-50:height,:]
+        hsv = cv2.cvtColor(hsv, cv2.COLOR_BGR2HSV)
 
+        # lower mask (0-10)
+        lower_red = np.array([0,50,50])
+        upper_red = np.array([10,255,255])
+        mask0 = cv2.inRange(hsv, lower_red, upper_red)
+
+        # upper mask (170-180)
+        lower_red = np.array([170,50,50])
+        upper_red = np.array([180,255,255])
+        mask1 = cv2.inRange(hsv, lower_red, upper_red)
+
+        mask = mask0 + mask1
+        mask_sum = np.sum(mask)
+        print("red mask: " + str(mask_sum))
+        #threshold for action - close enough to crosswalk
+        start_sum = False
+        see_sum = False
+        if mask_sum > 1000000:
+            start_sum = True
+        if mask_sum > 1:
+            see_sum = True
+        
+        return (start_sum, see_sum)
+
+    def safe_to_go(self, image):
+        hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+
+        #mask
+        lower_blue = np.array([100,150,0])
+        upper_blue = np.array([140,255,255])
+        mask = cv2.inRange(hsv, lower_blue, upper_blue)
+        mask = mask[:,0:420]
+        cv2.imshow("blue mask walk", mask)
+        cv2.waitKey(3)
+
+        mask_sum = np.sum(mask)
+
+        print("blue mask: {}".format(mask_sum))
+        if mask_sum < 200:
+            return True
+        return False
         
       
 
